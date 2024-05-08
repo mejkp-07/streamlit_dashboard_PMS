29dbc86f752b37dd40b37dd00dda2099699c11df
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-08 11:42:18 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -37,7 +37,8 @@ with st.sidebar:
 
 # Heatmap
 def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
-    heatmap = alt.Chart(input_df).mark_rect().encode(
+    df_counts = input_df.groupby([input_y, input_x]).size().reset_index(name='count')
+    heatmap = alt.Chart(df_counts).mark_rect().encode(
             y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="received project ", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
             x=alt.X(f'{input_x}:O', axis=alt.Axis(title=" ", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
             color=alt.Color(f'max({input_color}):Q',
