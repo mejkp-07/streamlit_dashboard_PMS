9d0c89b963501f3824b7c48bee959f7dab213b68
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-08 16:05:46 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -40,7 +40,7 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
     df_counts = input_df.groupby([input_y, input_x]).size().reset_index(name='count')
     heatmap = alt.Chart(df_counts).mark_rect().encode(
             y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="received project ", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X(f'{input_x}:Q', axis=alt.Axis(title=" ", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+            x=alt.X(f'{input_x}:O', axis=alt.Axis(title=" ", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
             color=alt.Color(f'max({input_color}):N',
                              legend=None,
                              scale=alt.Scale(scheme=input_color_theme)),
@@ -62,7 +62,7 @@ def horizon_graph(input_df, input_x, input_y,input_color, input_color_theme):
       opacity = 0.6,
   ).encode(
       y=alt.Y(f'{input_y}:O',axis = alt.Axis(title ="  ", titleFontSize=18, titlePadding = 15, titleFontWeight=900,labelAngle=0)),
-      x=alt.X(f'{input_x}:O',axis = alt.Axis(title =" ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+      x=alt.X(f'{input_x}:Q',axis = alt.Axis(title =" ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
       color = alt.Color(f'{input_color}:N',
                         legend =None,
                         scale= alt.Scale(scheme= input_color_theme)),
