3c34613008b1aec4207e9846ffb280e7c603796a
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-09 11:18:53 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -70,8 +70,8 @@ def scatterplot(input_df, input_x, input_y,input_color, input_color_theme):
     horizon = alt.Chart(input_df).mark_circle(
       size = 80,
   ).encode(
-      y=alt.Y(f'{input_y}:O',axis = alt.Axis(title ="  ", titleFontSize=18, titlePadding = 15, titleFontWeight=900,labelAngle=0)),
-      x=alt.X(f'{input_x}:O',axis = alt.Axis(title =" ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+      y=alt.Y(f'{input_y}:O',axis = alt.Axis(title =" funding organisation ", titleFontSize=18, titlePadding = 15, titleFontWeight=900,labelAngle=0)),
+      x=alt.X(f'{input_x}:O',axis = alt.Axis(title =" Cdac Outlay ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
       color = alt.Color(f'{input_color}:N',
                         legend =None,
                         scale= alt.Scale(scheme= input_color_theme)),
@@ -193,7 +193,7 @@ col = st.columns((1.5, 2.5, 2), gap='medium')
 
 with col[1]:
     st.markdown("#### Total")
-    scatter = scatterplot(df_data, 'Group Name', 'CDAC Outlay', 'Group Name',selected_color_theme )
+    scatter = scatterplot(df_data,  'CDAC Outlay','Funding Organisation', 'Group Name',selected_color_theme )
     st.altair_chart(scatter, use_container_width=True)
     
     heatmap = make_heatmap(df_data,'str_category_name','Group Name', 'Group Name',selected_color_theme)
