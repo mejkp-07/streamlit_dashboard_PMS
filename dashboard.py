eff23f912f29a9b03d484d69d7d981b8a480cb21
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-10 11:31:30 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -31,7 +31,7 @@ with st.sidebar:
   receival =  st.selectbox('Reveival of project',df_data['Received Project'].unique())
   group_name = st.selectbox('Select the department',df_data['Group Name'].unique())
   # selected_department = st.sidebar.selectbox('Select Department', ['Overall'] + df_data['Group Name'].unique())
-  # department_filter = st.sidebar.selectbox('Select Department', ['All'] + list(df_data['Group Name'].unique()))
+  department_filter = st.sidebar.selectbox('Select Department', ['All'] + list(df_data['Group Name'].unique()))
 
 
   color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
@@ -203,7 +203,7 @@ with col[0]:
 
     # migrations_col = st.columns((0.2, 1, 0.2))
     # with migrations_col[1]:
-    department_filter = st.sidebar.selectbox('Select Department', ['All'] + list(df_data['Group Name'].unique()))
+    # department_filter = st.sidebar.selectbox('Select Department', ['All'] + list(df_data['Group Name'].unique()))
     st.markdown('#### Conversion rate ')
     donut = donut_chart(df_data,department_filter,'blue')
     st.altair_chart(donut, use_container_width = True )
