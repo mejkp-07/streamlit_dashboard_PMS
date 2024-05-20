6f3d1130afe89de007beec49793082e0d180ebca
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-20 14:04:11 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -66,30 +66,30 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
      
     return heatmap
 
-def scatterplot(input_df, input_x, input_y,input_color, input_color_theme):
+# def scatterplot(input_df, input_x, input_y,input_color, input_color_theme):
     
-    # df_counts = input_df.groupby([input_y, input_x]).size().reset_index(name='count')
-    horizon = alt.Chart(input_df).mark_circle(
-      size = 80,
-  ).encode(
-      y=alt.Y(f'{input_y}:O',axis = alt.Axis(title =" funding organisation ", titleFontSize=18, titlePadding = 15, titleFontWeight=900,labelAngle=0)),
-      x=alt.X(f'{input_x}:O',axis = alt.Axis(title =" Cdac Outlay ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-      color = alt.Color(f'{input_color}:N',
-                        legend =None,
-                        scale= alt.Scale(scheme= input_color_theme)),
-      tooltip=['Group Name','Received Project','Duration']
+#     # df_counts = input_df.groupby([input_y, input_x]).size().reset_index(name='count')
+#     horizon = alt.Chart(input_df).mark_circle(
+#       size = 80,
+#   ).encode(
+#       y=alt.Y(f'{input_y}:O',axis = alt.Axis(title =" funding organisation ", titleFontSize=18, titlePadding = 15, titleFontWeight=900,labelAngle=0)),
+#       x=alt.X(f'{input_x}:O',axis = alt.Axis(title =" Cdac Outlay ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+#       color = alt.Color(f'{input_color}:N',
+#                         legend =None,
+#                         scale= alt.Scale(scheme= input_color_theme)),
+#       tooltip=['Group Name','Received Project','Duration']
 
 
-      ).interactive(
-    ).properties(
-          width = 1100,
-          # height = 90
-      ).configure_axis(
-          labelFontSize=12,
-          titleFontSize=12
-      )
+#       ).interactive(
+#     ).properties(
+#           width = 1100,
+#           # height = 90
+#       ).configure_axis(
+#           labelFontSize=12,
+#           titleFontSize=12
+#       )
 
-    return horizon
+#     return horizon
 def barchart(input_df, input_x, input_y,input_color, input_color_theme):
     
     # df_counts = input_df.groupby([input_y, input_x]).size().reset_index(name='count')
@@ -257,8 +257,8 @@ with col[0]:
 
 with col[1]:
     st.markdown("#### Total")
-    scatter = scatterplot(df_data,  'CDAC Outlay','Funding Organization', 'Group Name',selected_color_theme )
-    st.altair_chart(scatter, use_container_width=True)
+    bar = barchart(df_data,  'CDAC Outlay','Funding Organization', 'Group Name',selected_color_theme )
+    st.altair_chart(bar, use_container_width=True)
 
     st.markdown('#### Projects in each category')
     heatmap = make_heatmap(df_data,'Group Name','str_category_name', 'Group Name',selected_color_theme)
@@ -268,10 +268,10 @@ with col[1]:
 
 
 
-with col[2]:
-    st.markdown('#### Projects vs CDAC outlay ')
-    bar = barchart(df_data,  'CDAC Outlay','Funding Organization', 'Group Name',selected_color_theme )
-    st.altair_chart(bar, use_container_width=True)
+# with col[2]:
+#     st.markdown('#### Projects vs CDAC outlay ')
+#     bar = barchart(df_data,  'CDAC Outlay','Funding Organization', 'Group Name',selected_color_theme )
+#     st.altair_chart(bar, use_container_width=True)
      
 
 #     st.dataframe(df_selected_year_sorted,
