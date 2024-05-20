5c17b2b4f8d0ca547b72730f03b3cc9d9f4ead9b
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-20 16:44:54 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -100,10 +100,10 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
 
 #     return horizon
 def barchart(input_df, input_x, input_y,input_color, input_color_theme):
-    max_outlay_df = input_df.groupby([input_y, input_color])[input_x].max().reset_index()
+    # max_outlay_df = input_df.groupby([input_y, input_color])[input_x].max().reset_index()
     
     # Group by 'input_y' again and sum the max outlay values
-    sum_max_outlay_df = max_outlay_df.groupby([input_y, input_color])[input_x].sum().reset_index()
+    sum_outlay_df = input_df.groupby([input_y, input_color])[input_x].sum().reset_index()
     
     
     # df_counts = input_df.groupby([input_y, input_x]).size().reset_index(name='count')
@@ -112,10 +112,14 @@ def barchart(input_df, input_x, input_y,input_color, input_color_theme):
     cornerRadiusTopRight=3
   ).encode(
       y=alt.Y(f'{input_y}:O',axis = alt.Axis(title =" funding organisation ", titleFontSize=18, titlePadding = 15, titleFontWeight=900,labelAngle=0)),
-      x=alt.X(f'{input_x}:O',axis = alt.Axis(title =" Cdac Outlay ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+      x=alt.X(f'{input_x}:Q',axis = alt.Axis(title =" Cdac Outlay ",titleFontSize=18, titlePadding=15, titleFontWeight=900)),
       color = alt.Color(f'{input_color}:N',
                         legend =None,
                         scale= alt.Scale(scheme= input_color_theme)),
+      order=alt.Order(
+            f'sum({input_x}):Q', 
+            sort='descending'
+        ),
       tooltip=[alt.Tooltip(f'{input_y}:N', title='CDAC Outlay'),
                  alt.Tooltip(f'{input_x}:N', title='Funding organisation'),
                  ]
@@ -273,7 +277,7 @@ with col[0]:
 
 with col[1]:
     st.markdown("#### Total")
-    bar = barchart(df_data,'Funding Organization','CDAC Outlay', 'Group Name',selected_color_theme )
+    bar = barchart(df_data,'CDAC Outlay', 'Funding Organization','Group Name',selected_color_theme )
     st.altair_chart(bar, use_container_width=True)
 
     st.markdown('#### Projects in each category')
