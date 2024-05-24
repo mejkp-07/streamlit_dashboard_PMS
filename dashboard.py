c02c21f548f7bde1ae845c0f5aa136227ba2601a
01-Sia <117347998+01-Sia@users.noreply.github.com>
2024-05-24 11:38:12 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -136,7 +136,7 @@ def barchart(input_df, input_x, input_y, input_color, input_color_theme, selecte
 
 def scatter_plot_with_regression(input_df, input_x, input_y,input_color_theme):
     input_df[input_x] = input_df[input_x].str.extract('(\d+)').astype(float)
-    input_df[input_y] = input_df[input_y].str.replace('₹', '').str.replace('Lakhs', '').astype(float)
+    input_df[input_y] = input_df[input_y].str.replace('₹', '').str.replace(' ', '').str.replace('Lakhs', '').astype(float)
     input_df = input_df.dropna(subset=[input_x, input_y])
     
     # input_df = input_df.dropna(subset=[input_x, input_y])
